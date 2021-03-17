43deeb9f68714f807197c7c44a8d86b6e0b01c3a
TrungSky <trungsky@trungsky.local>
2021-03-17 15:27:32 +0700

    ok

--- app.js
+++ app.js
@@ -6,20 +6,16 @@ import HomePage from "./routes/HomePage";
 import morgan from "morgan";
 dotenv.config();
 
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 8080;
 // app.use(morgan("combined"));
 
-
 // Route
 app.use("/api", RouterProduct);
 app.get("/", HomePage);
 
-
-
 // App listen
 app.listen(port, () => {
-  console.log("Server run at : http://localhost:" + port);
+  console.log("Server run at: http://localhost:" + port);
 });
