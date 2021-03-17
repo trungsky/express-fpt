db1bfb5a3bd142c18d5287c488bc7d847ad4463c
TrungSky <trungsky@trungsky.local>
2021-03-17 15:45:26 +0700

    edit product get by id

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,4 @@
+import { json } from "body-parser";
 import express from "express";
 
 const router = express.Router();
@@ -7,7 +8,10 @@ router.get("/products", (req, res) => {
 });
 
 router.get("/products/:id", (req, res) => {
-  res.send(`ID: ${req.params.id}`);
+  res.json({
+    id: req.params.id,
+    name: `Product ${req.params.id}`,
+  });
 });
 
 router.post("/products", (req, res) => {
